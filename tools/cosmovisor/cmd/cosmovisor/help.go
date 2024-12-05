ff020729d51cc64a32c2002eb780a8546512ccf4
Jeancarlo <jeancarlobarrios@gmail.com>
2024-12-05 16:05:08 -0500

    fix: fix cosmovisor minor import  errrors

--- tools/cosmovisor/cmd/cosmovisor/help.go
+++ tools/cosmovisor/cmd/cosmovisor/help.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/agoric-labs/cosmos-sdk/cosmovisor"
+	"cosmossdk.io/tools/cosmovisor"
 )
 
 // GetHelpText creates the help text multi-line string.
 func GetHelpText() string {
-	return fmt.Sprintf(`Cosmosvisor - A process manager for Cosmos SDK application binaries.
+	return fmt.Sprintf(`Cosmovisor - A process manager for Cosmos SDK application binaries.
 
 Cosmovisor is a wrapper for a Cosmos SDK based App (set using the required %s env variable).
 It starts the App by passing all provided arguments and monitors the %s/data/upgrade-info.json
@@ -18,7 +18,7 @@ the proposal. Cosmovisor interprets that data to perform an update: switch a cur
 and restart the App.
 
 Configuration of Cosmovisor is done through environment variables, which are
-documented in: https://github.com/agoric-labs/cosmos-sdk/tree/main/cosmovisor/README.md`,
+documented in: https://docs.cosmos.network/main/tooling/cosmovisor`,
 		cosmovisor.EnvName, cosmovisor.EnvHome,
 	)
 }
